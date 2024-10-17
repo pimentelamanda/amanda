34df441df15935408f1faa6552763cfabc1565fe
pimentelamanda <164514050+pimentelamanda@users.noreply.github.com>
2024-10-17 09:42:50 -0300

    Add files via upload

---
+++ historia.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'No Pré-Fundamental',
+    'Dia de sessão de fotos',
+    'No pré-fundamental, tinha os dias de sessões de fotos dos alunos, onde eles vestiam com fantasia para nossos pais comprarem e terem uma lembrança.'
+
+)
+
+criaCartao(
+    ' 1° ano ensino médio',
+    'Final do ano letivo',
+    'No ano passado, teve o encerramento do ano letivo, onde eram pras turmas se juntarem e terem comido juntos, mas acabou que cada turma ficou pro si próprios. '
+)
+
+criaCartao(
+    'Passeio',
+    'Passeio de Teatro',
+    'No 9° ano, em outbro tivemos o passeio para o teatro Guaira, onde eu e minhas amigas assistimos uma peça. '
+
+)
+
+criaCartao(
+    '4° ano',
+    'Minha festa Junina',
+    ' No quarto ano, a minha escola fez uma festa Junina onde tiveram danças e brincadeiras . '
+)
